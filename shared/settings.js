5b95d429ea2995eb8425862d130823c4ffadcc73
Beer van der Drift <beerdrift@gmail.com>
2016-01-04 22:26:02 +0100

    Removed CSRF secure setting since in Meteor SQLInjectSecure also handles CSRFInjectSecure.

--- shared/settings.js
+++ shared/settings.js
@@ -3,7 +3,6 @@ Settings = {
 	Security: {
 		encryptPasswords: true,
 		XSSSecure: true,
-		CSRFSecure: true,
 		SQLInjectSecure: true, //Passwords are only insecure if encryptPassword is also disabled
 		ParameterTampSecure: true 
 	}
