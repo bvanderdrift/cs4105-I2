4f162eb1b1508d4c6fe34c42ce52b6737413854e
Beer van der Drift <beerdrift@gmail.com>
2016-01-04 16:50:10 +0100

    Added XSS vulnerability which can be set using the Security Settings object.

--- client/home/home.js
+++ client/home/home.js
@@ -2,4 +2,10 @@ Template.home.helpers({
   messages: function(){
     return Messages.find({});
   }
+});
+
+Template.message.helpers({
+  xsssecure: function(){
+    return Settings.Security.XSSSecure;
+  }
 })
