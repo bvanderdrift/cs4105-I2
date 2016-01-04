e2620a9066b16b30cf0e3fd7e6e4358bd5519704
Beer van der Drift <beerdrift@gmail.com>
2016-01-04 17:18:01 +0100

    Users can now input new messages once logged in.

--- client/home/home.js
+++ client/home/home.js
@@ -8,4 +8,22 @@ Template.message.helpers({
   xsssecure: function(){
     return Settings.Security.XSSSecure;
   }
-})
+});
+
+Template.postMessageForm.events({
+  "submit .new-message": function onNewMessage (arg) {
+     var messageInputField = $(".message-input");
+     var messageInput = messageInputField.val();
+
+     Messages.insert(makeMessage(messageInput));
+
+     return false;
+  }
+});
+
+function makeMessage(text){
+  return {
+    author: Meteor.user().username,
+    message: text
+  }
+}
